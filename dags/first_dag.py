4c8a8fbc97ea96ab36ec712b5ea2b5d9233940af
dlpkmr98 <dlpkmr98@gmail.com>
2021-07-29 13:08:43 +0530

    added airflow dags

---
+++ dags/first_dag.py
@@ -0,0 +1,61 @@
+
+try:
+
+    from datetime import timedelta
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from datetime import datetime
+    import pandas as pd
+
+except Exception as ex:
+    print("error   {}".format(ex))
+
+
+# xcom -- cross communication
+def first_function_execute(**context):
+    print("first_function_execute   ")
+    name = context.get('name', '**key not found**')
+    print("********==> "+name)
+    #get task instance using ti or task_instance
+    #xcom - cross communicaition between tasks
+    context['ti'].xcom_push(
+        key='mykey', value=f"first_function_execute says Hello, {name} ")
+
+
+def second_function_execute(**context):
+    instance = context.get("ti").xcom_pull(key="mykey")
+    data = [{"name": "Dilip", "title": "Full Stack Software Engineer"}, {
+        "name": "Nishanth", "title": "Full Stack Software Engineer"}, ]
+    df = pd.DataFrame(data=data)
+    print('@'*66)
+    print(df.head())
+    print('@'*66)
+
+    print("I am in second_function_execute got value :{} from Function 1  ".format(instance))
+
+
+with DAG(
+        dag_id="first_dag",
+        schedule_interval="@daily",
+        default_args={
+            "owner": "Dilip",
+            "retries": 1,
+            "retry_delay": timedelta(minutes=5),
+            "start_date": datetime(2021, 1, 1),
+        },
+        catchup=False) as f:
+
+    first_function_execute = PythonOperator(
+        task_id="first_function_execute",
+        python_callable=first_function_execute,
+        provide_context=True,
+        op_kwargs={"name": "Dilip Kumar"}
+    )
+
+    second_function_execute = PythonOperator(
+        task_id="second_function_execute",
+        python_callable=second_function_execute,
+        provide_context=True
+    )
+
+    first_function_execute >> second_function_execute
---
+++ dags/hello.py
@@ -0,0 +1,39 @@
+try:
+
+    from datetime import timedelta
+    from airflow import DAG
+    from airflow.operators.python_operator import PythonOperator
+    from datetime import datetime
+    import pandas as pd
+
+except Exception as ex:
+    print("error   {}".format(ex))
+
+
+# create task
+def function1():
+    print("Hello...")
+    return "Hello..."
+
+
+# Following are defaults which will be used by Airflow
+default_args = {
+    'owner': 'Dilip',
+    'depends_on_past': False,
+    'start_date': datetime(2021, 4, 15),
+    'email': ['dlpkmr98@gmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+# create dag
+dag = DAG('hello', default_args=default_args)
+
+#Use the PythonOperator to execute Python callables.
+function1 = PythonOperator(
+    task_id="function1",
+    python_callable=function1,
+    dag=dag
+)
